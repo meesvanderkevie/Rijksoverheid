c6b0487cab2a66b3c527f6e0906900b268ddfdd0
Mees van der Kevie <mvanderkevie30@gmail.com>
2022-07-16 16:39:37 +0200

    lock and unlock command added.

---
+++ commands/moderation/lock.js
@@ -0,0 +1,26 @@
+const discord = require('discord.js');
+
+module.exports.run = async (client, message, args, prefix) => {
+    const geenPermsEmbed = new MessageEmbed()
+    .setTitle('Geen permissie om deze command uit te voeren.')
+    .setDescription(`${message.author} Je hebt niet de perms deze command uit te voeren. Dit komt omdat je geen stafflid bent, of omdat je geen toegang hier tot hebt.`)
+    if(!message.member.permissions.has("MANAGE_SERVER")) return message.channel.send({embeds: [geenPermsEmbed]});
+
+    await message.channel.permissionOverwrites.set([
+        {
+            id: message.guild.roles.cache.find(r => r.name === "Burger").id,
+            deny: ["SEND_MESSAGES"]
+        }
+    ]);
+
+    const lockEmbed = new discord.MessageEmbed()
+    .setTitle(`Channel is locked - By: ${message.user.id}`)
+    .setColor("RED")
+
+    await message.channel.send({embeds: [lockEmbed]})
+}
+
+module.exports.help = {
+    name: "lock",
+    aliases: ["lock"],
+}
---
+++ commands/moderation/unlock.js
@@ -0,0 +1,27 @@
+const discord = require('discord.js');
+
+module.exports.run = async (client, message, args, prefix) => {
+    const geenPermsEmbed = new MessageEmbed()
+    .setTitle('Geen permissie om deze command uit te voeren.')
+    .setDescription(`${message.author} Je hebt niet de perms deze command uit te voeren. Dit komt omdat je geen stafflid bent, of omdat je geen toegang hier tot hebt.`)
+        
+    if(!message.member.permissions.has("MANAGE_SERVER")) return message.channel.send({embeds: [geenPermsEmbed]});
+
+    await message.channel.permissionOverwrites.set([
+        {
+            id: message.guild.roles.cache.find(r => r.name === "Burger").id,
+            allow: ["SEND_MESSAGES"]
+        }
+    ]);
+
+    const unlockEmbed = new discord.MessageEmbed()
+    .setTitle("Channel is unlocked!")
+    .setColor("GREEN")
+
+    await message.channel.send({embeds: [unlockEmbed]})
+}
+
+module.exports.help = {
+    name: "unlock",
+    aliases: ["unlock"],
+}

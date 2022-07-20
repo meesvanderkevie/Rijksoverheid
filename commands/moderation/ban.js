6eeeba90c277649c649ebec464825c3824cd3b9e
Mees van der Kevie <mvanderkevie30@gmail.com>
2022-07-20 22:55:52 +0200

    updated commands.

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -6,6 +6,9 @@ module.exports.run = async (message, args, client, prefix) => {
     .setDescription(`${message.author} Je hebt niet de perms deze command uit te voeren. Dit komt omdat je geen stafflid bent, of omdat je geen toegang hier tot hebt.`)
 
     if(!message.member.permissions.has('BAN_MEMBERS')) return message.reply({embeds: [geenPermsEmbed]});
+
+    message.reply("Commando is in ontwikkeling.");
+
 }
 
 module.exports.help = {
--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -6,6 +6,8 @@ module.exports = async (client, message, args, prefix) => {
     .setDescription(`${message.author} Je hebt niet de perms deze command uit te voeren. Dit komt omdat je geen stafflid bent, of omdat je geen toegang hier tot hebt.`)
 
     if(!message.member.permissions.has('KICK_MEMBERS')) return message.reply({embeds: [geenPermsEmbed]});
+
+    message.reply("Commando is in ontwikkeling.");
 }
 
 module.exports.help = {
